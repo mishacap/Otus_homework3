cd4119453b8309f1918c426373e35e9297646950
mishacap <trustredrum@gmail.com>
2024-05-05 21:03:08 +0300

    calc fix for module

--- tools/calculation.py
+++ tools/calculation.py
@@ -1,8 +1,17 @@
 import json
+from csv import DictReader
+from files import CSV_FILE_PATH
 from files import JSON_RESULT_FILE_PATH
 
 with open(JSON_RESULT_FILE_PATH, "r") as json_file:
     data = json.load(json_file)
 
 num_users = len(data)
+
+with open(CSV_FILE_PATH, "r") as csv_file:
+    data = DictReader(csv_file)
+    num_books = 0
+    for row in data:
+        num_books +=1
+
 
