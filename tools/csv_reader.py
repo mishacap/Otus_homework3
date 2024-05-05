054cdeec171f11a7c010c5e260fee75575a9e784
mishacap <trustredrum@gmail.com>
2024-05-05 18:56:05 +0300

    fix variable in readers

--- tools/csv_reader.py
+++ tools/csv_reader.py
@@ -1,8 +1,8 @@
 from csv import DictReader
 from files import CSV_FILE_PATH
 
-with open(CSV_FILE_PATH, newline='') as f:
-    reader = DictReader(f)
+with open(CSV_FILE_PATH, newline='') as csv_file:
+    reader = DictReader(csv_file)
 
     # Итерируемся по данным делая из них словари
     for row in reader:
--- tools/json_reader.py
+++ tools/json_reader.py
@@ -1,8 +1,8 @@
 import json
 from files import JSON_FILE_PATH
 
-with open(JSON_FILE_PATH, "r") as f:
-    users = json.load(f)
+with open(JSON_FILE_PATH, "r") as json_file:
+    users = json.load(json_file)
 
 users_list = users
 
