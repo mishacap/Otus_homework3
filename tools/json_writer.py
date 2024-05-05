d4a2d14bcb30b821754db5a7669dddb4f1544786
mishacap <trustredrum@gmail.com>
2024-05-05 19:42:24 +0300

    fix json writer

--- tools/json_writer.py
+++
@@ -1,8 +0,0 @@
-import json
-from files import JSON_RESULT_FILE_PATH
-
-data = {}
-
-with open(JSON_RESULT_FILE_PATH, "w") as json_result_file:
-    save = json.dumps(data, indent=4)
-    json_result_file.write(save)
