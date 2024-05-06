d03119e2a54f54d03cb42be77bcc220cb3c0a0a2
mishacap <trustredrum@gmail.com>
2024-05-06 19:06:46 +0300

    makes result.json

--- tools/json_writer_3.py
+++ tools/json_writer_3.py
@@ -9,9 +9,24 @@ with open(JSON_RESULT_FILE_PATH, "r") as users:
 with open(JSON_BOOKS_FILE_PATH, "r") as books:
     data_books = json.load(books)
 
-print(data_users)
-# print(data_books)
-# books_to_move = data_books[:8]
-# print(books_to_move)
+book_index = 0
+
+for user in data_users[:14]:
+    books_array = user["books"]
+    books_to_move = data_books[book_index:book_index + 8]
+    books_array.extend(books_to_move)
+    book_index += 8
+
+for user in data_users[14:]:
+    books_array = user["books"]
+    books_to_move = data_books[book_index:book_index + 7]
+    books_array.extend(books_to_move)
+    book_index += 7
+
+with open(JSON_RESULT_FILE_PATH, "w") as users_file:
+    json.dump(data_users, users_file, indent=4)
+
+
+
 
 
