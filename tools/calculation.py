edefb24305b782002a4711c0159106ec9db4e930
mishacap <trustredrum@gmail.com>
2024-05-07 17:22:30 +0300

    pylint and flake8 fix

--- tools/calculation.py
+++ tools/calculation.py
@@ -12,10 +12,12 @@ with open(CSV_FILE_PATH, "r") as csv_file:
     data = DictReader(csv_file)
     num_books = 0
     for row in data:
-        num_books +=1
+        num_books += 1
 
 
 # The first 14 users will receive 8 books each and the rest will receive 7 books each.
 books_per_user = num_books // num_users
+print(f"Каждому юзеру нужно раздать по {books_per_user} книг")
 
 remainder_books = num_books % num_users
+print(f"Оставшиеся {remainder_books} книг можно распределить между первыми юзерами из списка")
